c29d0b58442f786c4def38181d41de8acc93601e
giuliobosco <giuliobva@gmail.com>
2020-08-18 16:02:32 +0200

    add utils/validator_test UserValidator

--- utils/validators_test.go
+++ utils/validators_test.go
@@ -1,10 +1,21 @@
 package utils
 
 import (
+	"bytes"
+	"encoding/json"
+	"net/http"
+	"net/http/httptest"
+	"strings"
 	"testing"
 
+	"github.com/gin-gonic/gin"
+	"github.com/giuliobosco/todoAPI/model"
 	"github.com/stretchr/testify/assert"
 )
+
+func getTestUser() model.User {
+	return model.User{Email: "a@b.ch", Password: "123qwe", Lastname: "lastname", Firstname: "firstname"}
+}
 
 func TestEmailValidator(t *testing.T) {
 	// working email
@@ -20,22 +31,93 @@ func TestEmailValidator(t *testing.T) {
 	}
 }
 
-/*
-func TestUserValidator(t *testing.T) {
-	users := []model.User{
-		{},
-		{Email: "a"},
-		{Email: "a", Password: "b"},
-		{Email: "a", Password: "b", Firstname: "c"},
-		{Email: "a", Password: "b", Firstname: "c", Lastname: "d"},
+// TestUserValidator full user with password
+func TestUserValidatorPassword(t *testing.T) {
+	gin.SetMode(gin.TestMode)
+
+	c, _ := gin.CreateTestContext(httptest.NewRecorder())
+
+	expectedUser := getTestUser()
+
+	jsonStr, _ := json.Marshal(expectedUser)
+	var jsonBytes = []byte(jsonStr)
+	req, _ := http.NewRequest("POST", "/", bytes.NewBuffer(jsonBytes))
+
+	c.Request = req
+
+	actualUser, err := UserValidator(c, true)
+
+	assert.Equal(t, &expectedUser, actualUser)
+	assert.Nil(t, err)
+}
+
+// TestUserValidatorNoPassword, user without password
+func TestUserValidatorNoPassword(t *testing.T) {
+	gin.SetMode(gin.TestMode)
+
+	c, _ := gin.CreateTestContext(httptest.NewRecorder())
+
+	expectedUser := getTestUser()
+	expectedUser.Password = ""
+
+	jsonStr, _ := json.Marshal(expectedUser)
+	var jsonBytes = []byte(jsonStr)
+	req, _ := http.NewRequest("POST", "/", bytes.NewBuffer(jsonBytes))
+
+	c.Request = req
+
+	userActual, err := UserValidator(c, false)
+
+	assert.Equal(t, &expectedUser, userActual)
+	assert.Nil(t, err)
+}
+
+// testUserValidatorErrors, automatically test error for validator (automated)
+func testUserValidatorErrors(t *testing.T, u model.User, missing []string, usePassword bool) {
+	gin.SetMode(gin.TestMode)
+
+	c, _ := gin.CreateTestContext(httptest.NewRecorder())
+
+	jsonStr, err := json.Marshal(u)
+	assert.Nil(t, err)
+
+	jsonBytes := []byte(jsonStr)
+
+	req, err := http.NewRequest("POST", "/", bytes.NewBuffer(jsonBytes))
+	assert.Nil(t, err)
+
+	c.Request = req
+
+	actualUser, err := UserValidator(c, usePassword)
+
+	assert.Nil(t, actualUser)
+
+	for _, v := range missing {
+		assert.True(t, strings.Index(err.Error(), v) >= 0, "Error message should contains:"+v)
+	}
+}
+
+// userValidatorTestUnit automation unit for user validator test
+type userValidatorTestUnit struct {
+	Userr       model.User
+	ErrorStrigs []string
+	UsePassword bool
+}
+
+// TestUserValidatorPasswordErrors auto test all possible errors for userValidator
+func TestUserValidatorPasswordErrors(t *testing.T) {
+	m := []userValidatorTestUnit{
+		{model.User{Email: "a@b.ch", Firstname: "first", Lastname: "last"}, []string{"password"}, true},
+		{model.User{Email: "a@b.ch", Firstname: "first"}, []string{"password", "Lastname"}, true},
+		{model.User{Email: "a@b.ch"}, []string{"password", "Lastname", "Firstname"}, true},
+		{model.User{}, []string{"password", "Lastname", "Firstname", "email"}, true},
+
+		{model.User{Email: "a@b.ch", Firstname: "first"}, []string{"Lastname"}, false},
+		{model.User{Email: "a@b.ch"}, []string{"Lastname", "Firstname"}, false},
+		{model.User{}, []string{"Lastname", "Firstname", "email"}, false},
 	}
 
-	for _, user := range users {
-		ok, err := UserValidator(user)
-		assert.False(t, ok, err)
+	for _, v := range m {
+		testUserValidatorErrors(t, v.Userr, v.ErrorStrigs, v.UsePassword)
 	}
-
-	user := model.User{Email: "giuliobva@gmail.com", Password: "b", Firstname: "c", Lastname: "d"}
-	ok, err := UserValidator(user)
-	assert.True(t, ok, err)
-}*/
+}
